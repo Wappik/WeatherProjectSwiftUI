5f8e2ed3470af2a359d43428149b05d16492a00f
Wappik <wappik4@gmail.com>
2022-08-05 20:11:17 +0300

    create Weather line

--- WeatherProjectSwiftUI/Views/Main/HomeView.swift
+++ WeatherProjectSwiftUI/Views/Main/HomeView.swift
@@ -22,7 +22,19 @@ struct HomeView: View {
                     .font(.largeTitle)
                 
                 VStack {
-                    Text("19°" + "\n" + "Mostly Clear")
+                    //Text("19°" + "\n" + "Mostly Clear")
+                    
+//                    Text("19°")
+//                        .font(.system(size: 96, weight: .thin))
+//                        .foregroundColor(.primary)
+//                    +
+//                    Text("\n ")
+//                    +
+//                    Text("Mostly Clear")
+//                        .font(.title3.weight(.semibold))
+//                        .foregroundColor(.secondary)
+                    
+                    Text(attributedString)
                     
                     Text("H:24°   L:18°")
                         .font(.title3.weight(.semibold))
@@ -32,6 +44,27 @@ struct HomeView: View {
             }
             .padding(.top, 51)
         }
+    }
+    
+    private var attributedString: AttributedString {
+        var string = AttributedString("19°" + "\n " + "Mostly Clear")
+        
+        if let temp = string.range(of: "19°") {
+            string[temp].font = .system(size: 96, weight: .thin)
+            string[temp].foregroundColor = .primary
+        }
+        
+        if let pipe = string.range(of: " | ") {
+            string[pipe].font = .title3.weight(.semibold)
+            string[pipe].foregroundColor = .secondary
+        }
+        
+        if let weather = string.range(of: "Mostly Clear") {
+            string[weather].font = .title3.weight(.semibold)
+            string[weather].foregroundColor = .secondary
+        }
+        
+        return string
     }
 }
 
