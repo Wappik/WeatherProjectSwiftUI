a7eb42ff7e9ea3fb1b4b07154d34fec22d65ee7b
Wappik <wappik4@gmail.com>
2022-08-05 17:35:52 +0300

    Initial Commit

---
+++ WeatherProjectSwiftUI/WeatherProjectSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherProjectSwiftUIApp.swift
+//  WeatherProjectSwiftUI
+//
+//  Created by Эдуард Дума on 05.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherProjectSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
